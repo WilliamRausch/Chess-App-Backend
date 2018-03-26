cc0b89a2325823d67c2b1c7ad09574c50f6aca3e
WilliamRausch <nolanrausch@Gmail.com>
2018-03-26 08:47:30 -0400

    enabled CORS

---
+++ src/main/java/io/javabrains/springbootstarter/config/ChessApiConfig.java
@@ -0,0 +1,24 @@
+package io.javabrains.springbootstarter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * This global configuration enables CORS requests from any origin to any endpoint in the application.
+ * By default, this allows HEAD, GET, and POST, so added the allowedMethods to include PUT and DELETE.
+ */
+
+
+@Configuration
+@EnableWebMvc
+public class ChessApiConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
