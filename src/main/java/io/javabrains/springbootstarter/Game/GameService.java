9acf17199cf227a2971b09ba4a662085e44b7e9b
WilliamRausch <nolanrausch@Gmail.com>
2018-04-03 01:49:09 -0400

    handling for duplicate games

--- src/main/java/io/javabrains/springbootstarter/Game/GameService.java
+++ src/main/java/io/javabrains/springbootstarter/Game/GameService.java
@@ -42,37 +42,43 @@ public class GameService {
     }
 
     public void addGame(Game game) {
+        int id = game.getId();
 
-        for (int i = 0; i < 8; i++) {
-            game.getBoard().getspace(i, 1).setPiece(new Piece(i, 1, "black", "[P]", true));
-            game.getBoard().getspace(i, 6).setPiece(new Piece(i, 6, "white", "[p]", true));
+        if(gameRepository.findOne(id) == null) {
+
+            for (int i = 0; i < 8; i++) {
+                game.getBoard().getspace(i, 1).setPiece(new Piece(i, 1, "black", "[P]", true));
+                game.getBoard().getspace(i, 6).setPiece(new Piece(i, 6, "white", "[p]", true));
+            }
+            //Rooks
+            game.getBoard().getspace(0, 0).setPiece(new Piece(0, 0, "black", "[R]", true));
+            game.getBoard().getspace(7, 0).setPiece(new Piece(7, 0, "black", "[R]", true));
+            game.getBoard().getspace(0, 7).setPiece(new Piece(0, 7, "white", "[r]", true));
+            game.getBoard().getspace(7, 7).setPiece(new Piece(7, 7, "white", "[r]", true));
+
+            //Knights
+            game.getBoard().getspace(1, 0).setPiece(new Piece(1, 0, "black", "[N]", true));
+            game.getBoard().getspace(6, 0).setPiece(new Piece(6, 0, "black", "[N]", true));
+            game.getBoard().getspace(1, 7).setPiece(new Piece(1, 7, "white", "[n]", true));
+            game.getBoard().getspace(6, 7).setPiece(new Piece(6, 7, "white", "[n]", true));
+
+            //Bishops
+            game.getBoard().getspace(2, 0).setPiece(new Piece(2, 0, "black", "[B]", true));
+            game.getBoard().getspace(5, 0).setPiece(new Piece(5, 0, "black", "[B]", true));
+            game.getBoard().getspace(2, 7).setPiece(new Piece(2, 0, "white", "[b]", true));
+            game.getBoard().getspace(5, 7).setPiece(new Piece(5, 0, "white", "[b]", true));
+
+            //Queens
+            game.getBoard().getspace(3, 0).setPiece(new Piece(3, 0, "black", "[Q]", true));
+            game.getBoard().getspace(3, 7).setPiece(new Piece(3, 7, "white", "[q]", true));
+
+            //Kings
+            game.getBoard().getspace(4, 0).setPiece(new Piece(4, 0, "black", "[K]", true));
+            game.getBoard().getspace(4, 7).setPiece(new Piece(4, 7, "white", "[k]", true));
+            gameRepository.save(game);
+        }else{
+            System.out.println("ERROR");
         }
-        //Rooks
-        game.getBoard().getspace(0, 0).setPiece(new Piece(0, 0, "black", "[R]", true));
-        game.getBoard().getspace(7, 0).setPiece(new Piece(7, 0, "black", "[R]", true));
-        game.getBoard().getspace(0, 7).setPiece(new Piece(0, 7, "white", "[r]", true));
-        game.getBoard().getspace(7, 7).setPiece(new Piece(7, 7, "white", "[r]", true));
-
-        //Knights
-        game.getBoard().getspace(1, 0).setPiece(new Piece(1, 0, "black", "[N]", true));
-        game.getBoard().getspace(6, 0).setPiece(new Piece(6, 0, "black", "[N]", true));
-        game.getBoard().getspace(1, 7).setPiece(new Piece(1, 7, "white", "[n]", true));
-        game.getBoard().getspace(6, 7).setPiece(new Piece(6, 7, "white", "[n]", true));
-
-        //Bishops
-        game.getBoard().getspace(2, 0).setPiece(new Piece(2, 0, "black", "[B]", true));
-        game.getBoard().getspace(5, 0).setPiece(new Piece(5, 0, "black", "[B]", true));
-        game.getBoard().getspace(2, 7).setPiece(new Piece(2, 0, "white", "[b]", true));
-        game.getBoard().getspace(5, 7).setPiece(new Piece(5, 0, "white", "[b]", true));
-
-        //Queens
-        game.getBoard().getspace(3, 0).setPiece(new Piece(3, 0, "black", "[Q]", true));
-        game.getBoard().getspace(3, 7).setPiece(new Piece(3, 7, "white", "[q]", true));
-
-        //Kings
-        game.getBoard().getspace(4, 0).setPiece(new Piece(4, 0, "black", "[K]", true));
-        game.getBoard().getspace(4, 7).setPiece(new Piece(4, 7, "white", "[k]", true));
-        gameRepository.save(game);
 
     }
 
